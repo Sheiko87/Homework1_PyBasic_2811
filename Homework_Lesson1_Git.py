8a0eac393eb1c8cb01b73e491e641c96faa43705
Sheiko87 <nshejko29@gmail.com>
2022-12-04 16:30:57 +0100

    Task_2

--- Homework_Lesson1_Git.py
+++ Homework_Lesson1_Git.py
@@ -1,8 +1,16 @@
-variable_1="import antigravity"
+variable_1 = "import antigravity"
 print(variable_1)
-variable_2="import __hello__"
+variable_2 = "import __hello__"
 print(variable_2)
-variable_3="from __future__ import braces"
+variable_3 = "from __future__ import braces"
 print(variable_3)
 
+my_name = "*\t\t * \t\t * \t\t * * * * *\t\t *" \
+          "\n* *  \t * \t\t* *\t\t\t * \t\t\t* *" \
+          "\n*  * \t * \t  *\t\t*  \t\t * \t\t  *\t\t*" \
+          "\n*\t*\t * \t * * * * * \t\t * \t\t * * * * *" \
+          "\n*\t *\t * \t*\t\t  *\t\t * \t\t*\t\t  *" \
+          "\n*\t  *  * *\t\t   *\t * \t   *\t\t   *" \
+          "\n*\t\t*** \t\t\t* \t *\t  *\t\t\t\t*"
 
+print(my_name)
