66d8170928bcb6ea1250b710b2c507e17f2c8af2
Sheiko87 <nshejko29@gmail.com>
2022-12-04 16:53:45 +0100

    Task_3

--- Homework_Lesson1_Git.py
+++ Homework_Lesson1_Git.py
@@ -14,3 +14,18 @@ my_name = "*\t\t * \t\t * \t\t * * * * *\t\t *" \
           "\n*\t\t*** \t\t\t* \t *\t  *\t\t\t\t*"
 
 print(my_name)
+
+variable_bell="\\a\t\tBell (alert)"
+print(variable_bell)
+variable_backsp="\\b\t\tBackspace"
+print(variable_backsp)
+variable_new_line="\\n\t\tNew Line"
+print(variable_new_line)
+variable_hor_tab="\\t\t\tHorizontal tab"
+print(variable_hor_tab)
+variable_backsl="\\\ \t\tBackslash \\"
+print(variable_backsl)
+variable_double_mallards="\\\" \t\tDouble quotation mark \""
+print(variable_double_mallards)
+variable_single_mallards="\\\' \t\tSingle quotation mark \'"
+print(variable_single_mallards)
