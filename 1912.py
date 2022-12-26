068b9ebf0063f4ac64424808c327cfc45e19d114
Sheiko87 <nshejko29@gmail.com>
2022-12-26 12:03:36 -0500

    treygolnik

--- 1912.py
+++ 1912.py
@@ -1,33 +1,57 @@
 # 1
 my_string = '0123456789'
-unit = 0
-num = 0
-k = 0  # счетчик повторений цикла
-n = 0
-p = 0  # счетчик цифр
-while k <= 10:
-    for j in range(0, 10):
-        num = int(str(my_string)[n])
-        k = k + 1
-        result = unit + num
-        n = n + 1
-        unit = unit + 1
-        print(result, end=' ')
+for edinicu in my_string:
+    for desatki in my_string:
+        res = int(edinicu + desatki)
+        print(res, end=' ')
     print()
 
-    # else:
-    # unit=unit+1
-    # p=1
-    # for i in range(p, 10):
-    # num = int(str(my_string)[n])
-    # k = k + 1
-    # result = unit + num
-    # n = n + 1
-    # print(result, end=' ')
-    # if n < 11:
-    #   p=p+1
-    #  print(result, end=' ')
-pass
-
 # 2
-high = int(input('Введите высоту фигуры: '))
+# B
+
+strings = int(input('Введите высоту фигуры: '))
+for s in range(1, strings + 1):
+    for space in range(strings * 2 - s * 2):
+        print('-', end='')
+    for star in range(s * 2 - 1):
+        print('*', end=' ')
+    print()
+
+# A
+#            *
+#          *   *
+#        *       *
+#      *           *
+#    *               *
+#  *                   *
+# * * * * * * * * * * * * *
+
+strings = int(input('Введите высоту фигуры: '))
+p = 3
+for s in range(1, strings + 1):
+    for space in range(strings * 2 - s * 2):
+        print('-', end='')
+    if 2<=s<=6:
+        for star in range(2):
+            print('*','-'*(p-1), end='')
+        p=p+4
+    else:
+        for star in range(s * 2 - 1):
+            print('*', end=' ')
+    print()
+
+# ромб
+strings = int(input('Введите высоту фигуры: '))
+for s in range(0,strings+1):
+    for j in range(0,strings-s):
+        print('-',end='')
+    for j in range(0,s):
+        print('*',end=' ')
+    print()
+if s==strings:
+    for s in range(strings-1,0,-1):
+        for j in range(0,strings-s):
+            print('-',end='')
+        for j in range(0,s):
+            print('*',end=' ')
+        print()
