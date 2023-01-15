1108d7a0f2da5596c46474c2ce125acde87cbf0b
Sheiko87 <nshejko29@gmail.com>
2023-01-15 14:44:40 -0500

    all function

--- 0901.py
+++ 0901.py
@@ -1,5 +1,37 @@
+import Function_0901
 # 1
-#my_list=['jft','nhyr','sfth','vgt']
-from Function_0901.py import print_strings
-print_strings(['jft','nhyr','sfth','vgt'])
 
+from Function_0901 import print_strings
+
+print(print_strings(['jft', 'nhyr', 'sfth', 'vgt']))
+# 2
+
+from Function_0901 import print_list
+
+print(print_list(['fgkf', 'anncnb', 'jbvka', 'anf']))
+
+# 3
+from Function_0901 import letter_a
+
+print(letter_a(['ahdbn', 'hfdv', 'fhgga', 'gajja', 'hfvjk']))
+
+# 4
+
+from Function_0901 import strings_type
+
+print(strings_type(['cggf', 25, 78, 'cfgh']))
+
+# 5
+from Function_0901 import one_time_symbol
+
+print(one_time_symbol('jgjfkfdsjgfha'))
+
+# 6
+from Function_0901 import print_symbol
+
+print(print_symbol('jhfd', 'jhgfdar'))
+
+# 7
+from Function_0901 import symbol_double_str
+
+print(symbol_double_str('vdhjs', 'dhhmn'))
--- Function_0901.py
+++ Function_0901.py
@@ -1,12 +1,81 @@
 # 1
+def print_strings(strings):
+    my_list = []
+    for index in range(len(strings)):
+        if index % 2 != 0:
+            my_list.append(strings[index][::-1])
+        else:
+            my_list.append(strings[index])
+    return my_list
 
-def print_strings(list):
-    for index in range(len(list)):
-        for element in list:
-            if index % 2 != 0:
-                new_element = element[::-1]
-                list.append(new_element)
-            else:
-                list.append(element)
-    print(list)
-    print()
+
+# print(print_strings(['jft', 'nhyr', 'sfth', 'vgt', 'gfh']))
+
+
+# 2
+def print_list(string):
+    my_list = list()
+    for element in string:
+        for word in element:
+            if word[0] == 'a':
+                my_list.append(word)
+    #        print(my_list)
+    return my_list
+
+
+# print(print_list(['fgkf', 'anncnb', 'jbvka', 'anf']))
+
+# 3
+def letter_a(words):
+    my_list = []
+    for element in words:
+        if 'a' in element:
+            my_list.append(element)
+    return my_list
+
+
+# print(letter_a(['ahdbn','hfdv','fhgga','gajja','hfvjk']))
+
+# 4
+def strings_type(string_list):
+    my_list = []
+    for element in string_list:
+        if isinstance(element, str):
+            my_list.append(element)
+    return my_list
+
+
+# print(strings_type(['cggf', 25, 78, 'cfgh']))
+
+# 5
+def one_time_symbol(my_str):
+    my_list = []
+    for i in my_str:
+        if my_str.count(i) == 1:
+            my_list.append(i)
+    return my_list
+
+
+# print(one_time_symbol('jgjfkfdsjgfha'))
+
+# 6
+def print_symbol(str1, str2):
+    my_list = []
+    for i in str1:
+        if i in str2:
+            my_list.append(i)
+    return my_list
+
+
+# print(print_symbol('jhfd', 'jhgfdar'))
+
+# 7
+def symbol_double_str(str1, str2):
+    my_list = []
+    for i in str1:
+        if i in str2:
+            if str1.count(i) == 1 and str2.count(i) == 1:
+                my_list.append(i)
+    return my_list
+
+#print(symbol_double_str('vdhjs','dhhmn'))
